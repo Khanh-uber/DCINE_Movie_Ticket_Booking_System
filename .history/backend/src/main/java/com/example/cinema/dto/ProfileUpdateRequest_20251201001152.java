e232e1b9ef1b1f14ed8d4b7bcf0482aace5b29e3
Developer_2 <thucthcsll2@gmail.com>
2025-12-01 01:15:10 +0700

    Cap nhat profile

---
+++ .history/backend/src/main/java/com/example/cinema/dto/ProfileUpdateRequest_20251201001152.java
@@ -0,0 +1,10 @@
+package com.example.cinema.dto;
+
+public class ProfileUpdateRequest {
+    private String fullName;
+    private String phone;
+    private String dob;      // FE gửi string -> BE convert sang LocalDate
+    private String gender;   // MALE / FEMALE / OTHER
+    private String address;
+    
+}
---
+++ .history/backend/src/main/java/com/example/cinema/repository/AccountRepository_20251201002818.java
@@ -0,0 +1,26 @@
+package com.example.cinema.repository;
+
+import com.example.cinema.entity.Account;
+
+import io.lettuce.core.dynamic.annotation.Param;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.*;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByUsername(String username);
+    Account findByEmail(String email);  
+    Account findByPhone(String phone);
+    
+
+    Account find
+
+    @Query(value="""
+            select a.created_at, a.account_id, c.full_name, a.username, a.email, c.phone, c.dob, c.gender, c.address, a.avatar_url
+            from account a
+            join customer c on c.customer_id = a.customer_id
+            where account_id = :accountId
+            """, nativeQuery = true)
+    Map<String, Object> getUserInfo(@Param("accountId") Long accountId);
+}
