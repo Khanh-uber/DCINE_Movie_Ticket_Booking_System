10c930aea11ce8ec8c74c1d8b2412da16089f2ee
LTKhuong <lamthuykhuong1411@gamil.com>
2025-12-01 02:19:50 +0700

    cuuThuc

--- backend/database/dcine_data.sql
+++ backend/database/dcine_data.sql
@@ -3,7 +3,7 @@
 -- Host: localhost    Database: dcine_schema
 -- ------------------------------------------------------
 -- Server version	8.0.43
-
+use dcine_schema;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
