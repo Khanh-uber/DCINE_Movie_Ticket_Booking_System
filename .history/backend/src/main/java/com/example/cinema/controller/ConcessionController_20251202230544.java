4051f00b3ea0f1607e9532ebf9616f59a03efbba
Developer_2 <thucthcsll2@gmail.com>
2025-12-03 02:02:09 +0700

    cap nhat ok

---
+++ .history/backend/src/main/java/com/example/cinema/controller/ConcessionController_20251202230544.java
@@ -0,0 +1,50 @@
+package com.example.cinema.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cinema.dto.ConcessionCartRequest;
+import com.example.cinema.dto.ConcessionMeruRespose;
+import com.example.cinema.dto.ConcessionResponse;
+import com.example.cinema.service.ConcessionService;
+
+
+
+@RestController
+@RequestMapping("/api")
+public class ConcessionController {
+    private final ConcessionService concessionService;
+    private Long getAccountId(){
+        Long accountId = 1L;
+        return accountId;
+    }
+    public ConcessionController(ConcessionService concessionService){
+        this.concessionService = concessionService;
+    }
+    @GetMapping("/concessions")
+    public ResponseEntity<ConcessionMeruRespose> getMenu() {
+        return ResponseEntity.ok(concessionService.getMenu());
+    }
+
+    @GetMapping("/checkoust/ummary")
+    public ResponseEntity<ConcessionResponse> getSummary() {
+        Long accountId = getAccountId();
+        ConcessionResponse response = concessionService.loadSummary(accountId);
+
+        return ResponseEntity.ok(response);
+    }
+    @PostMapping("/concessions/cart")
+    public ResponseEntity<ConcessionResponse> updateCart(
+            @RequestBody ConcessionCartRequest req
+    ) {
+        Long accountId = getAccountId();
+        ConcessionResponse res = concessionService.updateCart(req, accountId);
+        return ResponseEntity.ok(res);
+    }
+
+    
+}
